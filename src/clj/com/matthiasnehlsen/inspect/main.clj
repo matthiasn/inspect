0e8848362bf3475750c822f78b76941679c517b6
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-10 00:17:05 +0100

    cleanup

--- src/clj/com/matthiasnehlsen/inspect/main.clj
+++ src/clj/com/matthiasnehlsen/inspect/main.clj
@@ -13,10 +13,10 @@
   [interval msg-type msg]
   (go-loop [] (<! (timeout interval)) (inspect msg-type msg) (recur)))
 
-(interval-put-loop  1000 :interval-put {:origin :every-second :payload {:msg "every second"}})
-(interval-put-loop  5000 :interval-put {:origin :every-five-seconds :payload {:msg "every five seconds"}})
-(interval-put-loop 10000 :interval-put {:origin :every-ten-seconds :payload {:msg "every ten seconds"}})
-(interval-put-loop 60000 :interval-put {:origin :every-minute :payload {:msg "every minute"}})
+(interval-put-loop  1000 :interval-put/every-second {:msg "every second"})
+(interval-put-loop  5000 :interval-put/every-five-seconds {:msg "every five seconds"})
+(interval-put-loop 10000 :interval-put/every-ten-seconds {:msg "every ten seconds"})
+(interval-put-loop 60000 :interval-put/every-minute {:msg "every minute"})
 
 (defn reload [] (inspect/stop) (refresh) (inspect/start))
 
