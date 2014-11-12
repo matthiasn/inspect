d52497c7568039c5e20c36743ec209bdd8a2ed96
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-12 22:54:41 +0100

    styling; ui experiments

--- src/cljs/core.cljs
+++ src/cljs/core.cljs
@@ -48,7 +48,7 @@
 (defn subscribed-selected
   "creates table with the current subscriptions"
   [client-map]
-  [:div
+  [:div.pure-u-md-1-4
    [:table.pure-table.table-small.pure-table-striped
     [:thead [:tr [:th "origin"] [:th "remaining"]]]
     [:tbody
@@ -92,6 +92,10 @@
 
 (defn run []
   (reagent/render-component (fn [] [inspect-view])
-                            (by-id "code")))
+                            (by-id "code"))
+  (reagent/render-component (fn [] [subscribed-selected appstate/client-map])
+                            (by-id "subscription-table"))
+  (reagent/render-component (fn [] [subscribed-selected appstate/client-map])
+                            (by-id "subscription-table2")))
 
 (run)
