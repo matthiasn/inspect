fd68c75bb0c36d2a5ed5dd016ca2a3adadc9b125
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-12 15:44:53 +0100

    example application

---
+++ example/src/example/core.clj
@@ -0,0 +1,25 @@
+(ns example.core
+  (:gen-class)
+  (:require
+   [com.matthiasnehlsen.inspect :as inspect :refer [inspect]]
+   [clojure.tools.namespace.repl :refer [refresh]]
+   [clojure.tools.logging :as log]
+   [clojure.pprint :as pp]
+   [clojure.core.async :as async :refer [<! chan put! timeout go-loop]]))
+
+(defn interval-put-loop
+  "put msg on chan every interval milliseonds"
+  [interval msg-type msg]
+  (go-loop [] (<! (timeout interval)) (inspect msg-type msg) (recur)))
+
+(interval-put-loop     1 :interval-put/every-millisecond {:msg "every millisecond"})
+(interval-put-loop  1000 :interval-put/every-second {:msg "every second"})
+(interval-put-loop  5000 :interval-put/every-five-seconds {:msg "every five seconds"})
+(interval-put-loop 10000 :interval-put/every-ten-seconds {:msg "every ten seconds"})
+(interval-put-loop 60000 :interval-put/every-minute {:msg "every minute"})
+
+(defn reload [] (inspect/stop) (refresh) (inspect/start))
+
+(defn -main [& args]
+  (log/info "Application started")
+  (inspect/start))
