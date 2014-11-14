c47564856c0567d58392af7d05cf89a5d2db2cf8
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-14 10:29:03 +0100

    published version 0.1.1 to clojars

--- example/project.clj
+++ example/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.matthiasnehlsen/inspect "0.1.1-SNAPSHOT"]
+                 [com.matthiasnehlsen/inspect "0.1.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :main ^:skip-aot example.core)
