ef91107de4e77038a322d3bd6a9681ac25e538fa
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-15 16:13:45 +0100

    bump to 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.matthiasnehlsen/inspect "0.1.2"
+(defproject com.matthiasnehlsen/inspect "0.1.3-SNAPSHOT"
   :description "Log to a web application to inspect what's going on in your application"
   :url "https://github.com/matthiasn/inspect"
   :license {:name "Eclipse Public License"
