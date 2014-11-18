42b9c1acd90854abafa0ca1d1cb7905538d9ce4d
Matthias Nehlsen <mn@nehlsen-edv.de>
2014-11-18 01:15:09 +0100

    mult and tap instead of "active" atom
    core.async provides better building blocks for dealing with the situation
    of starting and stopping a component by allowing to tap into a mult and
    untap from it again when closed.

--- src/clj/com/matthiasnehlsen/inspect.clj
+++ src/clj/com/matthiasnehlsen/inspect.clj
@@ -8,26 +8,24 @@
    [clojure.pprint :as pp]
    [clj-time.core :as t]
    [clj-time.format :as f]
-   [clojure.core.async :as async :refer [<! chan put! timeout go-loop sliding-buffer]]))
+   [clojure.core.async :as async :refer [<! chan put! mult tap pub sub timeout go-loop sliding-buffer]]))
 
-(def in-chan (chan (sliding-buffer 1000)))
+(def in-chan (chan (sliding-buffer 10000)))
+(def event-mult (mult in-chan))
 
 (def built-in-formatter (f/formatters :date-time))
-
-(def active (atom false))
 
 (defn inspect
   "Send message to inspect sub-system with msg-type. Only does anything when system active"
   [msg-type msg]
-  (when @active
-    (put! in-chan {:origin msg-type :received (f/unparse built-in-formatter (t/now)) :payload msg})))
+  (put! in-chan [:event {:origin msg-type :received (f/unparse built-in-formatter (t/now)) :payload msg}]))
 
 (defn get-system
   "Create system by wiring individual components so that component/start
   will bring up the individual components in the correct order."
   [conf]
   (component/system-map
-   :matcher (matcher/new-matcher in-chan inspect)
+   :matcher (matcher/new-matcher event-mult inspect)
    :http    (component/using (http/new-http-server conf) {:matcher :matcher})))
 
 ;; system with default port
@@ -41,11 +39,9 @@
 (defn start
   "start the inspect system and set active atom to true"
   []
-  (alter-var-root #'system component/start)
-  (reset! active true))
+  (alter-var-root #'system component/start))
 
 (defn stop
   "stop the inspect system and set active atom to false"
   []
-  (alter-var-root #'system (fn [s] (when s (component/stop s))))
-  (reset! active false))
+  (alter-var-root #'system (fn [s] (when s (component/stop s)))))
--- src/clj/com/matthiasnehlsen/inspect/matcher.clj
+++ src/clj/com/matthiasnehlsen/inspect/matcher.clj
@@ -6,7 +6,7 @@
    [clojure.core.match :as match :refer (match)]
    [taoensso.sente :as sente]
    [com.stuartsierra.component :as component]
-   [clojure.core.async :as async :refer [chan <! put! go-loop]]))
+   [clojure.core.async :as async :refer [chan <! put! tap untap-all sub unsub-all go-loop]]))
 
 (defn- user-id-fn
   "generates unique ID for request"
@@ -18,6 +18,7 @@
 (def clients (atom {}))
 (def client-maps (atom {}))
 (def known-event-types (atom #{}))
+(def stats (atom {}))
 
 (defn strip-uid
   "for some reason, the :client-uuid is longer than expected. This strips the
@@ -27,10 +28,8 @@
 
 (defn get-next-items
   "start listener for all event types, limited to the next n for each"
-  [client-map full-event]
-  (log/info "client-map" client-map)
-  (let [uid (strip-uid (:client-uuid full-event))]
-    (swap! client-maps assoc-in [uid] client-map)))
+  [inspect-chan client-map full-event]
+  (put! inspect-chan [:next (strip-uid (:client-uuid full-event)) client-map]))
 
 (defn initialize-inspector
   "start listener for all event types, limited to the next n for each"
@@ -43,60 +42,71 @@
 
 (defn close-connection
   "cleanup after connection was closed"
-  [client-uuid]
+  [inspect-chan client-uuid]
   (log/info "Connection closed:" client-uuid)
-  (swap! client-maps dissoc client-uuid))
+  (put! inspect-chan [:close client-uuid]))
 
 (defn send-event-types
   "send set with known event types to connected UIs"
   [uids chsk-send!]
   (doseq [uid (:any @uids)]
-    (chsk-send! uid [:info/known-event-types @known-event-types])))
+    (chsk-send! uid [:info/known-event-types @known-event-types])
+    (chsk-send! uid [:info/stats @stats])))
 
 (defn- make-handler
   "create event handler function for the websocket connection"
-  [inspect-fn chsk-send! uids]
+  [inspect-fn inspect-chan chsk-send! uids]
   (fn [full-event]
     (let [event (:event full-event)]
       (inspect-fn :ws/event-in full-event)
       (match event
-             [:cmd/get-next-items params] (get-next-items params full-event)
+             [:cmd/get-next-items params] (get-next-items inspect-chan params full-event)
              [:cmd/initialize params]     (initialize-inspector params full-event chsk-send!)
              [:cmd/get-event-types]       (send-event-types uids chsk-send!)
              [:chsk/ws-ping]              () ; currently just do nothing with ping (no logging either)
              [:chsk/uidport-open]         () ; user-id-fn already logs established connection
-             [:chsk/uidport-close]        (close-connection (strip-uid (:client-uuid full-event)))
+             [:chsk/uidport-close]        (close-connection inspect-chan (strip-uid (:client-uuid full-event)))
              :else                        (log/debug "Unmatched event:" event)))))
 
-(defn send-loop
+(defn add-stats
+  ""
+  [uids origin chsk-send!]
+  (swap! stats assoc origin (inc (get @stats origin 0)))
+  (when-not (contains? @known-event-types origin)
+    (swap! known-event-types conj origin)
+    (send-event-types uids chsk-send!)))
+
+(defn deliver-msg
+  ""
+  [uids msg chsk-send!]
+  (let [origin (:origin msg)]
+    (doseq [uid (:any @uids)]
+      (when (pos? (get-in @client-maps [uid origin] 0))
+        (chsk-send! uid [:info/msg (assoc msg :payload (with-out-str (pp/pprint (:payload msg))))])
+        (swap! client-maps #(update-in % [uid origin] dec))
+        (chsk-send! uid [:info/client-map (get-in @client-maps [uid])])))))
+
+(defn event-loop
   "run loop, call chsk-send! with message on channel"
   [channel uids chsk-send!]
-  (go-loop [] (let [msg (<! channel)
-                    origin (:origin msg)]
-                (when-not (= msg :stop-loop)
-                  (when-not (contains? @known-event-types origin)
-                    (swap! known-event-types conj origin)
-                    (send-event-types uids chsk-send!))
-                  (doseq [uid (:any @uids)]
-                    (when (pos? (get-in @client-maps [uid origin] 0))
-                      (chsk-send! uid [:info/msg (assoc msg :payload (with-out-str (pp/pprint (:payload msg))))])
-                      (swap! client-maps
-                             (fn [prev]
-                               (let [client-map (get-in prev [uid origin])]
-                                 (if client-map ;; check if exists, may have been changed
-                                   (update-in prev [uid origin] dec)
-                                   prev))))
-                      (chsk-send! uid [:info/client-map (get-in @client-maps [uid])])))
-                  (recur)))))
+  (go-loop [] (match (<! channel)
+                     [:event ev] (do
+                                   (add-stats uids (:origin ev) chsk-send!)
+                                   (deliver-msg uids ev chsk-send!))
+                     [:close uid]  (swap! client-maps dissoc uid)
+                     [:next uid client-map] (swap! client-maps assoc-in [uid] client-map))
+           (recur)))
 
-(defrecord Matcher [inspect-chan inspect-fn chsk-router]
+(defrecord Matcher [event-mult inspect-fn chsk-router]
   component/Lifecycle
   (start [component] (log/info "Starting Communicator Component")
-         (let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn connected-uids]}
+         (let [inspect-chan (chan 1000)
+               {:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn connected-uids]}
                (sente/make-channel-socket! {:user-id-fn user-id-fn})
-               event-handler (make-handler inspect-fn send-fn connected-uids)
+               event-handler (make-handler inspect-fn inspect-chan send-fn connected-uids)
                chsk-router (sente/start-chsk-router! ch-recv event-handler)]
-           (send-loop inspect-chan connected-uids send-fn)
+           (tap event-mult inspect-chan)
+           (event-loop inspect-chan connected-uids send-fn)
            (assoc component
              :ajax-post-fn ajax-post-fn
              :ajax-get-or-ws-handshake-fn ajax-get-or-ws-handshake-fn
@@ -104,11 +114,14 @@
 
   (stop [component] (log/info "Stopping Communicator Component")
         (chsk-router) ;; stops router loop
-        (put! inspect-chan :stop-loop)
+        (untap-all event-mult)
         (assoc component
           :chsk-router nil
           :ajax-post-fn nil
           :ajax-get-or-ws-handshake-fn nil)))
 
-(defn new-matcher [inspect-chan inspect-fn] (map->Matcher {:inspect-chan inspect-chan
-                                                           :inspect-fn inspect-fn}))
+(defn new-matcher
+  ""
+  [event-mult inspect-fn]
+  (map->Matcher {:event-mult event-mult
+                 :inspect-fn inspect-fn}))
