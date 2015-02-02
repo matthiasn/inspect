212cde5461836f5af97556c238bb0d2a0885891c
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-02-02 17:26:45 +0100

    Version bumps: [com.taoensso/sente "1.3.0"] and [org.clojure/clojurescript "0.0-2760"]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.matthiasnehlsen/inspect "0.1.5"
+(defproject com.matthiasnehlsen/inspect "0.1.6-SNAPSHOT"
   :description "Log to a web application to inspect what's going on in your application"
   :url "https://github.com/matthiasn/inspect"
   :license {:name "Eclipse Public License"
@@ -8,14 +8,14 @@
                  [org.clojure/tools.logging "0.3.0"]
                  [org.clojure/tools.namespace "0.2.7"]
                  [ch.qos.logback/logback-classic "1.1.1"]
-                 [com.taoensso/sente "1.2.0" :exclusions [org.clojure/clojure]]
+                 [com.taoensso/sente "1.3.0"]
                  [org.clojure/core.match "0.2.1"]
                  [http-kit "2.1.19"]
                  [compojure "1.2.1"]
                  [ring "1.3.1"]
                  [ring/ring-defaults "0.1.1"]
                  [clj-time "0.8.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/clojurescript "0.0-2760"]
                  [reagent "0.4.3"]
                  [com.stuartsierra/component "0.2.2"]]
 
