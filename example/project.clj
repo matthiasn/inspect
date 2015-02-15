7ec2f82da9bf1c8040292d4633a1d717d018a0b0
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-02-15 13:12:31 +0100

    v0.1.8 with updated dependencies

--- example/project.clj
+++ example/project.clj
@@ -1,10 +1,10 @@
-(defproject example "0.1.7"
+(defproject example "0.1.8"
   :description "Example Application for Inspect Library"
   :url "https://github.com/matthiasn/inspect"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.matthiasnehlsen/inspect "0.1.7"]
+                 [com.matthiasnehlsen/inspect "0.1.8"]
                  [clj-pid "0.1.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
--- project.clj
+++ project.clj
@@ -1,27 +1,27 @@
-(defproject com.matthiasnehlsen/inspect "0.1.8-SNAPSHOT"
+(defproject com.matthiasnehlsen/inspect "0.1.8"
   :description "Log to a web application to inspect what's going on in your application"
   :url "https://github.com/matthiasn/inspect"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/tools.logging "0.3.0"]
-                 [org.clojure/tools.namespace "0.2.7"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.namespace "0.2.9"]
                  [ch.qos.logback/logback-classic "1.1.1"]
                  [com.taoensso/sente "1.3.0"]
                  [org.clojure/core.match "0.2.1"]
                  [http-kit "2.1.19"]
-                 [compojure "1.2.1"]
-                 [ring "1.3.1"]
-                 [ring/ring-defaults "0.1.1"]
-                 [clj-time "0.8.0"]
+                 [compojure "1.3.1"]
+                 [ring "1.3.2"]
+                 [ring/ring-defaults "0.1.4"]
+                 [clj-time "0.9.0"]
                  [org.clojure/clojurescript "0.0-2850"]
                  [reagent "0.5.0-alpha3"]
                  [com.stuartsierra/component "0.2.2"]]
 
   :source-paths ["src/clj/"]
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.0.4"]]
 
   :cljsbuild {:builds [{:id "release"
                         :source-paths ["src/cljs"]
