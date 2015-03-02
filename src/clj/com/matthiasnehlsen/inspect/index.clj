4ce3e069ddb52947f46e276d24edce9cdca2199f
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-02 01:05:21 +0100

    additional CSS rules moved to Garden

--- src/clj/com/matthiasnehlsen/inspect/index.clj
+++ src/clj/com/matthiasnehlsen/inspect/index.clj
@@ -2,25 +2,52 @@
   (:gen-class)
   (:require
     [hiccup.core :refer [html]]
-    [garden.core :refer [css]]))
+    [garden.core :refer [css]]
+    [garden.units :as u :refer [px]]))
 
 (defn index-css
   "Generate index page CSS in Clojure using Garden."
   []
-  (css {:pretty-print? false} 
-       [:.active          {:font-weight :bold :color :black}]
-       [:.delimiter       {:font-weight :bold :color :red}]
-       [:.number          {:color "#6897BB"}]
-       [:.tag             {:color :red}]
-       [:.boolean         {:color :green}]
-       [:.class-delimiter {:color "#4471FF"}]
-       [:.string          {:font-weight :bold :color "#6A8759"}]
-       [:.character       {:font-weight :bold :color "#6A8759"}]
-       [:.keyword         {:font-weight :bold :color "#CC7832"}]
-       [:.class-name      {:font-weight :bold :color "#4471FF"}]
-       [:.function-symbol {:font-weight :bold :color "#4471FF"}]
-       [:.nil             {:font-weight :bold :color "#CC7832"}]
-       [:.symbol]))
+  (let [small-font (u/em 0.7)
+        string-color "#6A8759"
+        strong-blue "#4471FF"]
+    (css {:pretty-print? false}
+         [:.event
+          [:h4 {:margin-top 0
+                :margin-bottom 0
+                :color "#63809C"}]
+          [:pre {:margin-top (px 2)
+                 :background-color "#202F41"
+                 :color "#A9B7C6"
+                 :overflow :scroll-x
+                 :border-radius (px 2)
+                 :padding (px 10)}
+           [:code {:margin-top 0
+                   :margin-bottom (px 10)
+                   :font-size small-font
+                   :font-family "Monaco, monospace"
+                   :overflow :scroll-x}]]]
+         [:.received {:margin-top 0
+                      :margin-bottom 0
+                      :font-size small-font
+                      :float :right}]
+         [:table :th :td {:text-align :right :background-color "#EEE"}]
+         [:th {:background-color "#DDD"}]
+         [:.table-small     {:font-size small-font}]
+         [:.button-xsmall   {:font-size small-font :margin (px 2)}]
+         [:.active          {:font-weight :bold :color :black}]
+         [:.delimiter       {:font-weight :bold :color :red}]
+         [:.number          {:color "#6897BB"}]
+         [:.tag             {:color :red}]
+         [:.boolean         {:color :green}]
+         [:.class-delimiter {:color strong-blue}]
+         [:.string          {:font-weight :bold :color string-color}]
+         [:.character       {:font-weight :bold :color string-color}]
+         [:.keyword         {:font-weight :bold :color "#CC7832"}]
+         [:.class-name      {:font-weight :bold :color strong-blue}]
+         [:.function-symbol {:font-weight :bold :color strong-blue}]
+         [:.nil             {:font-weight :bold :color "#CC7832"}]
+         [:.symbol])))
 
 (defn index-page
   "Generates index page HTML with the specified page title."
