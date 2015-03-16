767be4be9bfcd3d4908445a0a61bfb66fbeda6d7
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-16 22:39:35 +0100

    fix for lein clean

--- project.clj
+++ project.clj
@@ -28,6 +28,8 @@
   :plugins [[lein-cljsbuild "1.0.5"]
             [codox "0.8.10"]]
 
+  :clean-targets ^{:protect false} ["resources/public/inspect/js/build/"]
+
   :cljsbuild {:builds [{:id "release"
                         :source-paths ["src/cljs"]
                         :compiler {:output-to "resources/public/inspect/js/build/inspect-opt.js"
