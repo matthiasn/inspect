447861548ea173833982372393ac62ac0e811cbf
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-12-16 00:12:40 +0100

    separate project for probe

--- src/clj/matthiasn/inspect/kafka_consumer.clj
+++ src/clj/matthiasn/inspect/kafka_consumer.clj
@@ -36,7 +36,7 @@
               (let [messages (kcz/messages c (:topic broker-config))]
                 (doseq [msg messages]
                   (let [thawed (nippy/thaw (:value msg))]
-                    (log/info thawed)
+                    ;(log/info thawed)
                     (put-fn thawed)
                     #_(apply inspect-fn thawed))))))
           {:state (atom {:consumer c})})))))
