84ede096e05958ab1031281beaf5f91c7b84e04f
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-10-19 19:59:01 +0200

    better packaging

--- publish.sh
+++ publish.sh
@@ -4,5 +4,13 @@
 rm -rf ./dist
 lein dist
 
-export ELECTRON_BUILDER_COMPRESSION_LEVEL=3
-electron-builder --publish always -ml
+PLATFORMS=$1
+ELECTRON_BUILDER_COMPRESSION_LEVEL=3
+
+if [ "$2" == "release" ]; then
+  echo "Publishing Release"
+  electron-builder --publish always $1
+else
+  echo "Publishing Beta Version"
+  electron-builder -c electron-builder-beta.yml --publish always $1
+fi
